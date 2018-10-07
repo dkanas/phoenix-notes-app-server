737946c9ea7157f713ebed8c04b7373e970549ef
Daniel Kanas <dkanas95@gmail.com>
2018-10-07 17:22:34 +0200

    guardian error handler fix

--- lib/notes/accounts/error_handler.ex
+++ lib/notes/accounts/error_handler.ex
@@ -1,7 +1,8 @@
 defmodule Notes.Accounts.ErrorHandler do
   import Plug.Conn
 
-  def auth_error(_conn, _data, _reason, _opts) do
-    {:error, :unauthenticated}
+  def auth_error(conn, {type, _reason}, _opts) do
+    body = Poison.encode!(%{message: to_string(type)})
+    send_resp(conn, 401, body)
   end
 end
